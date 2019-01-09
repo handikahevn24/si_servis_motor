45ae5b7d43318782a6f083e34cd117678454b2db
Hevn <handikahevn24@gmail.com>
2019-01-09 11:21:20 +0700

    Initiallize Si Sistem Motor

---
+++ navbar.php
@@ -0,0 +1,46 @@
+<nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>SI Service Motor</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>Pilihan Menu</p>
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Data Master</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="http://localhost/si_service_motor/pelanggan/">Data Pelanggan</a>
+                        </li>
+                        <li>
+                            <a href="http://localhost/si_service_motor/motor/">Data Motor</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Transaksi</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="http://localhost/si_service_motor/transaksi/servis/">Service Motor</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+
+            <ul class="list-unstyled CTAs">
+                <li>
+                    <a href="#" class="download">Sistem Informasi Service Motor</a>
+                </li>
+            </ul>
+        </nav>
---
+++ services.php
@@ -0,0 +1,69 @@
+<?php
+
+class Service{
+	
+	private $connection;
+
+	function __construct()
+	{
+		$this->connect_db();
+	}
+
+	public function connect_db(){
+		$this->connection = mysqli_connect('localhost', 'root', '', 'si_servis_motor');
+		if(mysqli_connect_error()){
+			die("Database Connection Failed" . mysqli_connect_error() . mysqli_connect_errno());
+		}
+	}
+
+	public function create($fname,$lname,$email,$gender,$age){
+		$sql = "INSERT INTO `services` (firstname, lastname, email, gender, age) VALUES ('$fname', '$lname', '$email', '$gender', '$age')";
+		$res = mysqli_query($this->connection, $sql);
+		if($res){
+	 		return true;
+		}else{
+			return false;
+		}
+	}
+
+	public function read($id=null){
+		$sql = "SELECT * FROM `crud`";
+		if($id){ $sql .= " WHERE id=$id";}
+ 		$res = mysqli_query($this->connection, $sql);
+ 		return $res;
+	}
+
+	public function update($fname,$lname,$email,$gender,$age, $id){
+		$sql = "UPDATE `crud` SET firstname='$fname', lastname='$lname', email='$email', gender='$gender', age='$age' WHERE id=$id";
+		$res = mysqli_query($this->connection, $sql);
+		if($res){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public function delete($id){
+		$sql = "DELETE FROM `crud` WHERE id=$id";
+ 		$res = mysqli_query($this->connection, $sql);
+ 		if($res){
+ 			return true;
+ 		}else{
+ 			return false;
+ 		}
+	}
+
+	public function sanitize($var){
+		$return = mysqli_real_escape_string($this->connection, $var);
+		return $return;
+    }
+    
+    public function generateID(){
+        $sql = "SELECT id_ FROM `services` ";
+    }
+
+}
+
+$crud = new Crud();
+
+?>
